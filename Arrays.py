79f7b4dd64f32d9688e6ff7128365b254c43d192
Pallavi <pallavi3749@gmail.com>
2022-08-28 12:56:40 +0530

    arrays and tuples

---
+++ Arrays.py
@@ -0,0 +1,73 @@
+# marks =[100,20,30,60,90,50]
+# print(marks)
+# print(len(marks))
+# total=0
+# for i in range(0,len(marks)):
+#     total+=marks[i]
+
+# print(total)
+# total=0
+# for value in marks:
+#     total+=value
+# print(total)
+
+# def greet(name):
+#     print("Hello " ,name)
+
+# names=["John","Tony","Lily","Nancy"]
+
+# for n in names:
+#     greet(n)
+
+
+
+#some inbuilt functions
+
+# arr=[]
+# arr.append(30)
+# arr.append(10)
+# arr.append(20)
+# print(arr)
+# arr.sort()
+# print(arr)
+# arr.pop()
+# print(arr)
+# arr.insert(2,50)
+# print(arr)
+
+
+
+#Create an array of len 20
+
+# arr=[]
+# for i in range(0,20):
+#     arr.append(0)
+
+# print(len(arr))
+
+# #or
+# arr2 =[0]*20
+# print(len(arr2))
+
+
+#2D Arrays
+
+mat=[[0]*3]*2
+print(mat)
+print(len(mat))
+
+for i in range(2):
+    for j in range (3):
+        print(mat[i][j], end=" ")
+    print(" ")
+
+
+
+#Tuples
+
+record_1 =("soham",10)
+print(record_1)
+
+records=[("soham",20),("John",50)]
+print(records)
+
